004e6fd2581d28fdff7ba69cddc29ddd706ca5ac
Mohammad Mahdi Mohammadi <bluelifemmd2007@gmail.com>
2026-02-18 11:12:59 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from app.gui import run_gui
+
+if __name__ == "__main__":
+    run_gui()
+
+
+    print(">>> MAIN FILE LOADED")
+from app.gui import run_gui
+print(">>> IMPORT DONE")
+
+if __name__ == "__main__":
+    print(">>> MAIN BLOCK EXECUTED")
+    run_gui()
+
+
+
